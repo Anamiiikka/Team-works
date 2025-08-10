e2ba42e18110869087b695eec18e30fb211cce80
codedpool <codedpool10@gmail.com>
2025-08-10 15:49:17 +0530

    about us top

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,7 +1,24 @@
 import AboutUs from "@/components/AboutUs"
+import Services from "@/components/Services"
+import WhoWeAre from "@/components/WhoWeAre"
+import Testimonials from "@/components/Testimonials"
+import ContactUs from "@/components/ContactUs"
+import Footer from "@/components/Footer"
+import Top from "@/components/Top"
 
 export default function AboutPage() {
     return(
-        <AboutUs showFounders={true} />
-    )       
+        <>
+            <Top 
+            title="About Us"
+        locationTitle="Home >> About Us"
+            />
+            <AboutUs showFounders={false} />
+            <WhoWeAre />
+            <Services />
+            <Testimonials />
+            <ContactUs />
+            <Footer />
+        </>
+    )
 }
